62f45ca0a11452a2bc12f632cdec860e4579641b
javier171188 <javier171188@hotmail.com>
2021-09-23 13:59:47 -0500

    parser does not update context.js

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -59,7 +59,7 @@ const Provider = ({ children }) => {
                 setUserState(newState);
             });
         }
-        console.log(userState);
+        console.log('where?');
     }
     
     const value = {
