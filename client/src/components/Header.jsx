8aa46697cdea7ebe7fd7fdac16d8ec9fb060ee39
javier171188 <javier171188@hotmail.com>
2021-10-04 19:11:25 -0500

    Buttons to change laguage working

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,6 +5,12 @@ import { useTranslation } from 'react-i18next';
 
 const Header = () => {
     const { t, i18n } = useTranslation();
+    function changeLanguage(event){
+        event.preventDefault();
+        let language = event.target.id.split('-')[0];
+        console.log(language);
+        i18n.changeLanguage(language).then(v => console.log(v));
+    }
     return(
         <Context.Consumer>
             {
@@ -13,6 +19,9 @@ const Header = () => {
                         <header className='header'>
                         <h1 className='header-title'>{t('App name')}</h1>
                         <button onClick={logOut}> {t('Logout')}</button>
+                        <button onClick={changeLanguage} id="en-button"> {t('English')}</button>
+                        <button onClick={changeLanguage} id="es-button"> {t('Spanish')}</button>
+                        <button onClick={changeLanguage} id="fr-button"> {t('French')}</button>
                         </header>
                     )
                 }
