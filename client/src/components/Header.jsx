c829fd5b47e2b53337cd3d70fd6fbeb18fcba368
javier171188 <javier171188@hotmail.com>
2021-10-04 19:45:06 -0500

    Working on the menu to logout and go settings

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,10 +17,15 @@ const Header = () => {
                     return (
                         <header className='header'>
                         <h1 className='header-title'>{t('App name')}</h1>
-                        <button onClick={logOut}> {t('Logout')}</button>
-                        <button onClick={changeLanguage} id="en-button"> {t('English')}</button>
-                        <button onClick={changeLanguage} id="es-button"> {t('Spanish')}</button>
-                        <button onClick={changeLanguage} id="fr-button"> {t('French')}</button>
+                        <div className="header__menu">
+                            <div className="header__menu--profile">
+                                <p>H</p>
+                            </div>
+                            <ul>
+                                <li><a href="/">{t('Setting')}</a></li>
+                                <li><a href="/">{t('Logout')}</a></li>
+                            </ul>
+                        </div>
                         </header>
                     )
                 }
@@ -28,6 +33,10 @@ const Header = () => {
         </Context.Consumer>
         );
 }
-
-
 export default Header;
+
+/*<button onClick={logOut}> {t('Logout')}</button>
+                        <button onClick={changeLanguage} id="en-button"> {t('English')}</button>
+                        <button onClick={changeLanguage} id="es-button"> {t('Spanish')}</button>
+                        <button onClick={changeLanguage} id="fr-button"> {t('French')}</button>
+*/
