4ef3065c8bec9accd2675647e33193251eedf4f6
javier171188 <javier171188@hotmail.com>
2021-10-06 17:28:20 -0500

    language detection implemented

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -5,15 +5,28 @@ const resources = require('../locales/resources.json');
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 
-if (!localStorage.getItem('language')){
-  localStorage.setItem('language', 'en');
+
+let lang = localStorage.getItem('language');
+if (!lang){
+  lang = navigator.language;
+  if (lang){
+    lang = lang.split('-')[0];
+  }
 }
+
+if (!['en', 'es', 'fr'].includes(lang)){
+  lang = 'en';
+}
+
+
+
+
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: localStorage.getItem('language'), // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: lang, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
