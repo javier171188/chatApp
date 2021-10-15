5b94400442be4c260c076d46b3e59b970ae8b11b
javier171188 <javier171188@hotmail.com>
2021-10-14 19:44:59 -0500

    Working on deploy from yaml file

--- users/src/db/mongoose.js
+++ users/src/db/mongoose.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 const connectionURL =  process.env.DB_URL; 
 const databaseName = process.env.DB_NAME; 
-
+console.log(connectionURL);
+console.log(databaseName);
 mongoose.connect(`${connectionURL}/${databaseName}`, {
     useNewUrlParser: true,
     useCreateIndex: true,
