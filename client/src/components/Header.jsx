a97ad367686d3fa55b0c42475ff99480086c5e95
javier171188 <javier171188@hotmail.com>
2021-10-25 17:59:21 -0500

    Header is responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,21 +17,21 @@ const Header = () => {
                 ({ logOut }) => {
                     return (
                         <header className='header'>
-                        <div className="to-center">
-                            <div>
-                                <p> ...</p>
+                            <div className="to-center">
+                                <div>
+                                    <p> ...</p>
+                                </div>
                             </div>
-                        </div>
-                        <h1 className='header-title' onClick={goHome}>{t('App name')}</h1>
-                        <div className="header__menu">
-                            <div className="header__menu--button">
-                                <p className='header__menu--icon'> ...</p>
+                            <h1 className='header-title' onClick={goHome}>{t('App name')}</h1>
+                            <div className="header__menu">
+                                <div className="header__menu--button">
+                                    <p className='header__menu--icon'> ...</p>
+                                </div>
+                                <ul className='header__menu--options'>
+                                    <li ><Link className='header__menu--item'  to="/chat/settings/">{t('Settings')}</Link></li>
+                                    <li ><div className='header__menu--item' onClick={logOut}>{t('Logout')}</div></li>
+                                </ul>
                             </div>
-                            <ul className='header__menu--options'>
-                                <li ><Link className='header__menu--item'  to="/chat/settings/">{t('Settings')}</Link></li>
-                                <li ><div className='header__menu--item' onClick={logOut}>{t('Logout')}</div></li>
-                            </ul>
-                        </div>
                         </header>
                     )
                 }
