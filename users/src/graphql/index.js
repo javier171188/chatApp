1f20d768a213247a6717b9a35f15606e345bf579
javier171188 <javier171188@hotmail.com>
2021-11-19 12:43:34 -0600

    Basic example working

--- users/src/graphql/index.js
+++ users/src/graphql/index.js
@@ -1,20 +1,28 @@
 'use strict';
 
-const { graphql, buildSchema } = require('graphql');
+const { buildSchema } = require('graphql');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require('./resolvers');
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`)
+const app = express();
+const port = process.env.port || 3000;
+
+const schema = buildSchema(readFileSync(
+  join(__dirname, 'schema.graphql'),
+  'utf-8'
+));
 
 
-const resolvers = {
-    hello: () => {
-        return 'Hello world'
-    }
-}
 
-graphql(schema, '{hello}', resolvers).then((data) => {
-    console.log(data);
-}).catch(console.log)
+app.use('/api', graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
