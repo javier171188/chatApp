3ed8befe563624d1b4ecdb4b07c92be4b85dcc86
javier171188 <javier171188@hotmail.com>
2022-05-17 12:01:29 -0500

    Local stream implemented

--- client/src/components/Conference.jsx
+++ client/src/components/Conference.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import OvenLiveKit from 'ovenlivekit';
@@ -7,26 +8,24 @@ import OvenLiveKit from 'ovenlivekit';
 function Conference(props){
     const{currentRoomName} = props;
     const history = useHistory();
-
-
-    
     const localInput = OvenLiveKit.create();
-    // Get media stream from user device
-    localInput.getUserMedia().then(function () {
-            let localVideo = document.getElementById('local-video');
-            localInput.attachMedia(localVideo);
-            localVideo.srcObject = localInput.stream;
-            
-            
-            // Got device stream and start streaming to OvenMediaEngine
-            //localInput.startStreaming('wss://your_oven_media_engine:3333/app/stream?direction=send');
-    });
+    let localVideo;
 
-
+    useEffect(()=>{
+        localVideo = document.getElementById('local-video');
+        localInput.attachMedia(localVideo);
+        // Get media stream from user device
+        localInput.getUserMedia().then(function () {
+                localVideo.srcObject = localInput.stream;
+                // Got device stream and start streaming to OvenMediaEngine
+                //localInput.startStreaming('wss://your_oven_media_engine:3333/app/stream?direction=send');
+        });
+    }, [])
     
-
     function goHome(){
-        console.log('Remember to finish the call.')
+        console.log('Remember to finish all the calls.')
+        localVideo.srcObject = null;
+        localInput.remove();
         history.push('/chat/');
     }
     return (
