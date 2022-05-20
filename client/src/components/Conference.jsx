82f2b3975dd67b654157e59a4e29f3c1e697e8b6
javier171188 <javier171188@hotmail.com>
2022-05-20 12:43:58 -0500

    Conference seems to work with hls

--- client/src/components/Conference.jsx
+++ client/src/components/Conference.jsx
@@ -1,157 +1,167 @@
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-import {getRoomUsers, checkStreamsAction, setCurrentStreamsAction} from '../redux/actions';
-import OvenPlayer from 'ovenplayer';
-import OvenLiveKit from 'ovenlivekit';
+import {
+  getRoomUsers,
+  checkStreamsAction,
+  setCurrentStreamsAction,
+} from "../redux/actions";
+import OvenPlayer from "ovenplayer";
+import OvenLiveKit from "ovenlivekit";
 //require('hls.js');
 
 let localInput;
 let localVideo;
 let checkStreamsInterval;
 
-function Conference(props){
-    const{currentRoomName, 
-        userState, 
-        currentUsers, 
-        getRoomUsers, 
-        currentRoomId,
+function Conference(props) {
+  const {
+    currentRoomName,
+    userState,
+    currentUsers,
+    getRoomUsers,
+    currentRoomId,
+    checkStreamsAction,
+    currentStreams,
+    setCurrentStreamsAction,
+  } = props;
+  const { _id: userId } = userState;
+  const history = useHistory();
+  let streamIds = currentStreams.map((s) => s.split("-")[1]);
+
+  useEffect(() => {
+    getRoomUsers(currentRoomId);
+    localInput = OvenLiveKit.create();
+    localVideo = document.getElementById("local-video");
+    localInput.attachMedia(localVideo);
+    // Get media stream from user device
+    localInput.getUserMedia().then(function () {
+      localVideo.srcObject = localInput.stream;
+      // Got device stream and start streaming to OvenMediaEngine
+      localInput.startStreaming(
+        `ws://localhost:3333/app/${currentRoomId}-${userId}?direction=send&transport=tcp`,
+        {
+          maxVideoBitrate: 500,
+        }
+      );
+    });
+
+    if (!checkStreamsInterval) {
+      checkStreamsInterval = setInterval(
         checkStreamsAction,
-        currentStreams,
-        setCurrentStreamsAction,
-    } = props;
-    const {_id:userId} = userState;
-    const history = useHistory();
-    let streamIds = currentStreams.map(s => s.split('-')[1]);
-    
-    useEffect(()=>{
-        getRoomUsers(currentRoomId);
-        localInput = OvenLiveKit.create();
-        localVideo = document.getElementById('local-video');
-        //localInput.attachMedia(localVideo);
-        // Get media stream from user device
-        localInput.getUserMedia().then(function () {
-                //localVideo.srcObject = localInput.stream;
-                // Got device stream and start streaming to OvenMediaEngine
-                localInput.startStreaming(
-                    `ws://localhost:3333/app/${currentRoomId}-${userId}?direction=send&transport=tcp`,
-                    {
-                      maxVideoBitrate: 500,
-                    }
-                  );
-                
-        });
-
-        if (!checkStreamsInterval){
-            checkStreamsInterval = setInterval(checkStreamsAction, 2500,currentRoomId );
-        };
-
-    }, [])
-
-    
-
-
-    useEffect(()=>{
-        console.log('streams: ',streamIds);
-
-        streamIds.forEach(stId => {
-            if (stId === userId) return;
-            
-            let player = OvenPlayer.getPlayerByContainerId(`${currentRoomId}-${stId}`);
-
-            if (player) return;
-
-            player = OvenPlayer.create(`${currentRoomId}-${stId}`, {
-                            sources: [
-                                //         {
-                                //     label: 'label_for_webrtc',
-                                //     // Set the type to 'webrtc'
-                                //     type: 'webrtc',
-                                //     // Set the file to WebRTC Signaling URL with OvenMediaEngine 
-                                //     file: `ws://localhost:3333/app/${currentRoomId}-${stId}_bypass`
-                                // },   
-                                {type : "hls", 
-                                file :  `http://localhost:8080/app/${currentRoomId}-${stId}_bypass/playlist.m3u8`, 
-                                label: "hls"}
-                            ],
-                            autoStart: true,
-                        });
-        });
-
-        currentUsers.forEach(u =>{
-            if (!streamIds.includes(u._id)){
-                let player = OvenPlayer.getPlayerByContainerId(`${currentRoomId}-${u._id}`);
-
-                if (player){
-                    player.remove()
-                }
-            }
-        })
-
-    }, [currentStreams]);
-    
-    function goHome(){
-        console.log('Remember to clear the setInterval')
-        localVideo.srcObject = null;
-        localInput.remove();
-        //The documentation mentions stopStreaming, but it doesn't seem to be defined.
-        //localInput.stopStreaming();
-        clearInterval(checkStreamsInterval);
-        checkStreamsInterval = null;
-
-        currentUsers.forEach(u =>{
-                let player = OvenPlayer.getPlayerByContainerId(`${currentRoomId}-${u._id}`);
-
-                if (player){
-                    player.remove()
-                }
-            }
-        )
-
-        history.push('/chat/');
-        setCurrentStreamsAction([]);
+        2500,
+        currentRoomId
+      );
     }
+  }, []);
 
-    console.log('times');
-    
-    return (
-        <>
-            <h1>Call with {currentRoomName} </h1>
-            <video id='local-video'></video>
-            <button onClick={goHome}>Hang up!</button>
-            <div>
-                {streamIds.map((stream)=>{
-                    let currentUser =currentUsers.filter(u =>{
-                        console.log(stream);  
-                        return  u._id === stream})[0];
-                    
-                    let currentUserId = currentUser._id;
-                   if (currentUserId !== userId ){
-                    
-                    return <div key={currentUser._id}>
-                        <div id={`${currentRoomId}-${stream}`}></div>
-                        <div > {currentUser.userName}</div>
-                   </div>
-                   }
-                })}
-            </div>
-        </>
-    )
+  useEffect(() => {
+    console.log("streams: ", streamIds);
+
+    streamIds.forEach((stId) => {
+      if (stId === userId) return;
+
+      let player = OvenPlayer.getPlayerByContainerId(
+        `${currentRoomId}-${stId}`
+      );
+
+      if (player) return;
+
+      player = OvenPlayer.create(`${currentRoomId}-${stId}`, {
+        sources: [
+          {type : "hls",
+          file :  `http://localhost:8080/app/${currentRoomId}-${stId}_bypass/playlist.m3u8`,
+          label: "hls"},
+          {
+            label: "label_for_webrtc",
+            // Set the type to 'webrtc'
+            type: "webrtc",
+            // Set the file to WebRTC Signaling URL with OvenMediaEngine
+            file: `ws://localhost:3333/app/${currentRoomId}-${stId}_bypass`,
+          },
+         
+        ],
+        autoStart: true,
+      });
+    });
+
+    currentUsers.forEach((u) => {
+      if (!streamIds.includes(u._id)) {
+        let player = OvenPlayer.getPlayerByContainerId(
+          `${currentRoomId}-${u._id}`
+        );
+
+        if (player) {
+          player.remove();
+        }
+      }
+    });
+  }, [currentStreams]);
+
+  function goHome() {
+    console.log("Remember to clear the setInterval");
+    localVideo.srcObject = null;
+    localInput.remove();
+    //The documentation mentions stopStreaming, but it doesn't seem to be defined.
+    //localInput.stopStreaming();
+    clearInterval(checkStreamsInterval);
+    checkStreamsInterval = null;
+
+    currentUsers.forEach((u) => {
+      let player = OvenPlayer.getPlayerByContainerId(
+        `${currentRoomId}-${u._id}`
+      );
+
+      if (player) {
+        player.remove();
+      }
+    });
+
+    history.push("/chat/");
+    setCurrentStreamsAction([]);
+  }
+
+  console.log("times");
+
+  return (
+    <>
+      <h1>Call with {currentRoomName} </h1>
+      <video id="local-video"></video>
+      <button onClick={goHome}>Hang up!</button>
+      <div>
+        {streamIds.map((stream) => {
+          let currentUser = currentUsers.filter((u) => {
+            console.log(stream);
+            return u._id === stream;
+          })[0];
+
+          let currentUserId = currentUser._id;
+          if (currentUserId !== userId) {
+            return (
+              <div key={currentUser._id}>
+                <div id={`${currentRoomId}-${stream}`}></div>
+                <div> {currentUser.userName}</div>
+              </div>
+            );
+          }
+        })}
+      </div>
+    </>
+  );
 }
 
-
 const mapStateToProps = (state) => ({
-    currentRoomName: state.chatArea.currentRoomName,
-    currentRoomId: state.chatArea.currentRoomId,
-    userState: state.userState,
-    currentUsers: state.chatArea.currentUsers,
-    currentStreams: state.chatArea.currentStreams,
-  });
+  currentRoomName: state.chatArea.currentRoomName,
+  currentRoomId: state.chatArea.currentRoomId,
+  userState: state.userState,
+  currentUsers: state.chatArea.currentUsers,
+  currentStreams: state.chatArea.currentStreams,
+});
 
 const mapDispatchToProps = {
-    getRoomUsers,
-    checkStreamsAction,
-    setCurrentStreamsAction,
-  };
+  getRoomUsers,
+  checkStreamsAction,
+  setCurrentStreamsAction,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Conference);
